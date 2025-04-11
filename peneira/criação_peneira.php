8c73b01365f1d2508cd5eea00f58f747500dd95d
luciano <luciano.paiva.sousa.jr@gmail.com>
2025-04-11 20:27:49 -0300

    inicio da criação das peneiras

---
+++ peneira/criação_peneira.php
@@ -0,0 +1,24 @@
+<!--aba reservada para o clube que irá criar a peneira colocando suas informações que serão passadas para a pagina da peneira-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>criação da peneira</title>
+</head>
+<body>
+    <form>
+        <!--campo onde será definido o nome da peneira-->
+        <div class="div-nome-peneira">
+            <label for="exampleFormControlInput1" class="label-nome-peneira">Nome Peneira</label>
+            <input type="text" class="input-nome-peneira" id="id-nome-peneira" placeholder="Nome da Peneira">
+        </div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </form>
+</body>
+</html>
---
+++ peneira/peneira.php
@@ -0,0 +1,13 @@
+<!--pagina que recebera as informações da criação da peneira e será de livre acesso para os jogadores-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    ?>
+</body>
+</html>
