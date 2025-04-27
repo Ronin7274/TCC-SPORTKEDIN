010547b1deb5a8bccac84708b37846f0bd9ac718
luciano <luciano.paiva.sousa.jr@gmail.com>
2025-04-26 23:47:24 -0300

    continuacao da criacao do corpo da peneira

--- peneira/criação_peneira.php
+++ peneira/criação_peneira.php
@@ -1,39 +1,47 @@
 <!--aba reservada para o clube que irá criar a peneira colocando suas informações que serão passadas para a pagina da peneira-->
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>criação da peneira</title>
 </head>
+
 <body>
     <form method="POST" action="peneira.php">
         <!--campo onde será definido o nome da peneira-->
         <div class="div-nome-peneira">
             <label for="nome-peneira" class="label-nome-peneira">Nome Peneira</label>
-            <input type="text" class="input-nome-peneira" name="nome_peneira" id="nome-peneira" placeholder="Nome da Peneira">
+            <input type="text" class="input-nome-peneira" name="nome_peneira" id="nome-peneira" required placeholder="Nome da Peneira">
         </div>
+
         <!--campo onde será definida data de acontecimento da peneira-->
         <div calss="div-data-peneira">
             <label for="data-peneira" class="label-data-peneira">data do evento</label>
-            <input type="datetime-local" class="input-data-peneira" name="data_peneira" id="nome-peneira">
+            <input type="datetime-local" class="input-data-peneira" name="data_peneira" required id="nome-peneira">
         </div>
+
         <!--campo onde será definida a data de fechamento da peneira-->
         <div class="div-data-fechamento-peneira">
             <label for="data-fechamento-peneira" class="label-data-peneira">Data de fechamento da peneira</label>
-            <input type="datetime-local" class="input-data-fechamento-peneira" name="fechamento_peneira" id="data-fechamento-peneira">
+            <input type="datetime-local" class="input-data-fechamento-peneira" name="fechamento_peneira" required id="data-fechamento-peneira">
         </div>
+
         <!--campo onde será definida a quantidade de vagas da peneira-->
         <div class="div-vagas-peneira">
             <label for="vagas-peneira" class="label-vagas-peneira">Numero de vagas da peneira</label>
-            <input type="number" class="input-vagas-peneira" name="vagas_peneira" id="vagas-peneira">
+            <input type="number" class="input-vagas-peneira" name="vagas_peneira" required id="vagas-peneira">
         </div>
+
         <!--campo onde será definido o texto de descrição da peneira-->
         <div class="div-texto-peneira">
             <label for="texto-peneira" class="label-texo-peneira"></label>
-            <input type="text" class="input-texto-peneira" name="texto_peneira" id="texto-peneira" placeholder="Descrição da peneira">
+            <input type="text" class="input-texto-peneira" name="texto_peneira" id="texto-peneira" required placeholder="Descrição da peneira">
         </div>
         <button type="submit" name="criar-peneira" class="criar-peneira">Criar peneira</button>
     </form>
+
 </body>
+
 </html>
--- peneira/peneira.php
+++ peneira/peneira.php
@@ -1,29 +1,56 @@
 <!--pagina que recebera as informações da criação da peneira e será de livre acesso para os jogadores-->
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <?php
-        if($_SERVER["REQUEST_METHOD"] == "POST")
-        {
-            $nome_peneira = $_POST["nome_peneira"];
-            $data_peneira = $_POST["data_peneira"];
-            $fechamento_peneira = $_POST["fechamento_peneira"];
-            $texto_peneira = $_POST["texto_peneira"];
-            //garante que o numero recebido seja inteiro
-            $vagas_peneira = intval($_POST["vagas_peneira"]); 
-            $vagas = [];
-            //cria o array de 1 até o número digitado
-            for ($i = 1; $i <= $vagas_peneira; $i++){
-                $vagas[] = $i;
-            }
-            echo "nome: $nome_peneira, data: $data_peneira, fechamento: $fechamento_peneira, vagas: $vagas_peneira";
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $nome_peneira = $_POST["nome_peneira"];
+        $data_peneira = $_POST["data_peneira"];
+        $fechamento_peneira = $_POST["fechamento_peneira"];
+        $texto_peneira = $_POST["texto_peneira"];
+        //garante que o numero recebido seja inteiro
+        $vagas_peneira = intval($_POST["vagas_peneira"]);
+        $vagas = [];
+        //cria o array de 1 até o número digitado
+        for ($i = 1; $i <= $vagas_peneira; $i++) {
+            $vagas[] = $i;
         }
-
+        echo "nome: $nome_peneira, data: $data_peneira, fechamento: $fechamento_peneira, vagas: $vagas_peneira";
+    }
     ?>
+
+    <label class="teste_peneira"><?php echo "$nome_peneira"; ?></label>
+    <div>
+        <!--data de acontecimento da peneira-->
+        <label class="teste_peneira">Horario da peneira</label>
+        <input type="datetime-local" readonly class="teste_peneira" value="<?php echo "$data_peneira"; ?>">
+        <!--data fechamento peneira-->
+        <label class="teste_peneira">Horario de fechamento das inscricoes</label>
+        <input type="datetime-local" readonly  class="teste_peneira" value="<?php echo "$fechamento_peneira"; ?>">
+        <!--arrey que deve mostrar a quantidade total de vagas e a quantidade preenchidas-->
+        <label class="teste_peneira">Quantidade de vagas</label>
+        <input type="number"  readonly class="teste_peneira" value="<?php echo "$vagas" ?> / <?php echo "$vagas_peneira"; ?>">
+    </div>
+
+
+
+<!--css basico apenas para destacar os elementos de teste-->
+    <style>
+        .teste_peneira {
+            color: red;
+        }
+    </style>
+
+
+
+
 </body>
+
 </html>
